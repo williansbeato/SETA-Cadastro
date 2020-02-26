2d466e3f00d2a69dac03be112e85721684d4f5ee
williansbeato <willians_beato@hotmail.com>
2020-02-26 18:41:55 -0300

    CRUD - Turma - Views

---
+++ recursos/view/turma.php
@@ -0,0 +1,85 @@
+<?php
+
+    include_once '../../global.php';
+
+    if( !empty($_POST['form_submit']) ) {
+        CAluno::rota();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <link rel="icon" href="../img/favicon.ico">
+    <title>Sistema Acadêmico - DWII</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link href="../css/theme.css" rel="stylesheet">
+    <!-- Java Script -->
+    <script src="../js/jquery-3.3.1.slim.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+
+  </head>
+
+  <body role="document">
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand">Sistema Acadêmico - DWII</a>
+        </div>
+	<div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+              <li class="active">
+                   <a href="main.php"> Home </a>
+              </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container theme-showcase" role="main">
+
+        <div class="page-header">
+            <h2 class="form-signin-heading">
+                <div id="m_texto"> Turmas Cadastrados </div>
+            </h2>
+        </div>
+
+        <form class="form" method="post" action="curso.php">
+            <input TYPE="hidden" NAME="form_submit" VALUE="OK">
+
+            <button type="submit" name="acao" value="cadastrar/0" class="btn btn-primary btn-block">
+                <b>Cadastrar Nova Turma</b>
+            </button>
+            <br>
+            <table class='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>NOME</th>
+                        <th>ANO</th>
+                        <th>AÇÕES</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                     //
+                    ?>
+                </tbody>
+            </table>
+        </form>
+
+	<div class="page-header">
+		<b>&copy;2020&nbsp;&nbsp;&raquo;&nbsp;&nbsp; Will</b>
+	</div>
+    </div>
+  </body>
+</html>
